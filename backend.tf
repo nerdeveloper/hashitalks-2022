1911a78eaf39a2018f5e11d33710afb809974a8b
nerdeveloper <odirionye@gmail.com>
2022-04-07 02:01:20 +0400

    ci: update terraform ci

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     bucket      = "ps-workspace-remote-state-dev-gloml-us-west2-first"
     prefix      = "ps-workspace-dev"
-    credentials = "creds.json"
+    credentials = "service_account.json"
 
 
   }
