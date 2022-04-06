f1a81156c134c4c6d30b2b1e156ece9f50fa3e59
nerdeveloper <odirionye@gmail.com>
2022-04-07 02:45:21 +0400

    ci: finished

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     bucket = "ps-workspace-remote-state-dev-gloml-us-west2-first"
     prefix = "ps-workspace-dev"
-    #    credentials = "/Users/odirionye/Documents/terraform/hashitalks/2022/creds.json" # with atlantis
+    # credentials = "/Users/odirionye/Documents/terraform/hashitalks/2022/creds.json" # with atlantis
     credentials = "creds.json" // With Terraform
 
 
