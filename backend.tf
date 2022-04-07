42868af9f75379a3d65a47235859766cb9f97795
nerdeveloper <odirionye@gmail.com>
2022-04-07 14:32:02 +0400

    ci:

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "gcs" {
-    bucket = "ps-workspace-remote-state-dev-b6739-us-west2-first"
-    prefix = "ps-workspace-dev"
-    #    credentials = "creds.json"
-    credentials = "/Users/odirionye/Documents/terraform/hashitalks/2022/creds.json"
+    bucket      = "ps-workspace-remote-state-dev-b6739-us-west2-first"
+    prefix      = "ps-workspace-dev"
+    credentials = "creds.json"
+    #    credentials = "/Users/odirionye/Documents/terraform/hashitalks/2022/creds.json"
 
   }
 }
